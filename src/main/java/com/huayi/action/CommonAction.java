1cafadcfb9abe25c946f6e0f2280e96468ae6f6b
何万宁 <hhewanning@163.com>
2018-05-11 15:29:50 +0800

    路径跳转

--- src/main/java/com/huayi/action/CommonAction.java
+++ src/main/java/com/huayi/action/CommonAction.java
@@ -4,14 +4,8 @@ import org.apache.log4j.Logger;
 
 public class CommonAction {
 	private static final Logger LOGGER = Logger.getLogger(UserAction.class);
-	private String page;
-	public String goPage(){
-		return "";
-	}
-	public String getPage() {
-		return page;
-	}
-	public void setPage(String page) {
-		this.page = page;
+	public String goMainPage(){
+		LOGGER.info("去adminjsps/admin/main.jsp页面");
+		return "success";
 	}
 }
